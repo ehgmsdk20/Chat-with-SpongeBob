25128ba159e72412883c2ce57596140032b77e83
KimDoheun <ehgmsdk@kaist.ac.kr>
2021-06-24 09:38:28 +0900

    add warning message for spongebob api

--- static/js/index.js
+++ static/js/index.js
@@ -33,8 +33,11 @@ function onSendButtonClicked() {
 
 
     if (topic == '') {
-        document.getElementById('warning').innerText = 'Please fill topic!';
+        document.getElementById('input_warning').innerText = 'Please fill topic!';
         return ;
+    }
+    else {
+        document.getElementById('input_warning').innerText = '';
     }
     sendMessage(message, 'right');
     document.getElementById("message_input").value='';
@@ -46,7 +49,7 @@ function get_script(message, name, topic){
     var formData = new FormData(); 
     formData.append("name", "SpongeBob" );
     formData.append("text", message);
-    formData.append("length", 30);
+    formData.append("length", 50);
     const url = "https://master-gpt2-spongebob-fpem123.endpoint.ainize.ai/SpongeBob";
     fetchUntilCondition(url, formData, name, 0, response => { 
         callback(response, name, message, x => {
@@ -56,7 +59,7 @@ function get_script(message, name, topic){
     return ;
 }
 
-var maxnum=3; //The max num that repeats function "fetchUntilCondition"
+var maxnum=5; //The max num that repeats function "fetchUntilCondition"
 
 function fetchUntilCondition(url, formData, name, i, cb){
     fetch(
@@ -66,24 +69,28 @@ function fetchUntilCondition(url, formData, name, i, cb){
             body: formData
         } ).then(response => {
             if (response.status == 200){
+                document.getElementById('api_warning').innerText = '';
                 return response;
             }
             else{
-                throw Error("Failed");
+                throw 'Cannot access to SpongeBob API!';
             }
         })
         .then(response => response.json())
-        .then(json => json['0'])
+        .then(json => {
+            return json['0'];
+        })
         .then(result => {
             if(result.length<2 && i<maxnum) {
-                fetchUntilCondition(url, formData, name, i+1, cb);
+                fetchUntilCondition(url, formData, name, i+1, cb); // fetch again
             }
             else if(result[1][0]!=name && i<maxnum) {
                 fetchUntilCondition(url, formData, name, i+1, cb); // fetch again
             }
             else if(i==maxnum) {
-                if(result[1][0]==name){
-                    cb(result[1][1]);
+                if(result.length>1 && result[1][0]==name){
+                    message = result[1][1].replace(/\[[^\]]*\]/g, ''); // Remove parenthesized string in script
+                    cb(message);
                 }
                 else {
                     cb(0)
@@ -91,10 +98,14 @@ function fetchUntilCondition(url, formData, name, i, cb){
             }
             
             else {
-                cb(result[1][1]);
+                message = result[1][1].replace(/\[[^\]]*\]/g, '');
+                cb(message);
             }
         })
-        .catch(e => cb(0));
+        .catch(e => {
+            document.getElementById('api_warning').innerText = e;
+            cb(0);
+        });
 }
 
 function callback(response, name, message, cb) {
