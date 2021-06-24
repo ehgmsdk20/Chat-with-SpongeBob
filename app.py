5f1f8e7d52ef03d77ce12c005dca85f8e9e27125
KimDoheun <ehgmsdk@kaist.ac.kr>
2021-06-24 13:09:23 +0900

    update dockerfile

--- app.py
+++ app.py
@@ -4,4 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def main():
     return render_template('index.html')
-app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=False, host='0.0.0.0', port=80)
+
